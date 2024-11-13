05ced5442032ceea9f929b18ad0f5511c9543d10
Debiddo <dzheng@terpmail.umd.edu>
2024-11-12 23:44:02 -0500

    add background color to homepage

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList, Button } from "react-native";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  const startAddGoalHandler = () => {
+    setModalIsVisible(true);
+  };
+  const endAddGoalHandler = () => {
+    setModalIsVisible(false);
+  };
+
+  const deleteGoalHandler = (id) => {
+    setCourseGoals(courseGoals.filter((goal) => goal.id != id));
+  };
+
+  const addGoalHandler = (enteredGoalText) => {
+    setCourseGoals([
+      ...courseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+    endAddGoalHandler();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    <StatusBar style='light'/>
+    <View style={styles.appContainer}>
+      <Button
+        title="Add New Goal"
+        color={"#a065ec"}
+        onPress={startAddGoalHandler}
+      />
+      <GoalInput
+        onAddGoal={addGoalHandler}
+        visible={modalIsVisible}
+        onCancel={endAddGoalHandler}
+      />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          keyExtractor={(item, id) => {
+            return item.id;
+          }}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
+        />
+      </View>
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 15,
+    backgroundColor: "#1e085a"
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
---
+++ README.md
@@ -0,0 +1,8 @@
+# How To Create A React Native App
+
+## Step 1
+-npx create-expo-app@latest --template blank ProjectName
+-npx create-expo-app --template blank ProjectName
+
+## Step 2
+-npm start
